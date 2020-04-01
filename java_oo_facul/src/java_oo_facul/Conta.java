a9fee9fe200b8b7f9e17a579db429a9cbadbecf7
carlos <carlo@LAPTOP-4QT499AT>
2020-04-01 09:32:47 -0300

    Modificado - algumas mudanças

--- java_oo_facul/src/java_oo_facul/Conta.java
+++ java_oo_facul/src/java_oo_facul/Conta.java
@@ -4,25 +4,23 @@ public class Conta {
 	private double saldo;
 	private String numConta;
 	private String titular;
-	
-	
+
 	public Conta(double saldo, String numConta, String titular) {
 		this.saldo = saldo;
 		this.numConta = numConta;
 		this.titular = titular;
 	}
-	
+
 	public void imprimirDados() {
 		System.out.printf(
-				"Numero da conta: "+ numConta
-				+ "\nTitular: " + titular 
-				+ "\nSaldo: " +  String.format("%.2f", saldo));
+				"Numero da conta: " + numConta + "\nTitular: " + titular + "\nSaldo: " + String.format("%.2f", saldo));
 	}
 
 	public void deposito(double deposito) {
 		this.saldo += deposito;
-		
+
 	}
+
 	public void saque(double saque) {
 		this.saldo -= saque;
 	}
--- java_oo_facul/src/java_oo_facul/TesteConstructor.java
+++ java_oo_facul/src/java_oo_facul/TesteConstructor.java
@@ -3,63 +3,59 @@ package java_oo_facul;
 import java.util.Locale;
 import java.util.Scanner;
 
-
 public class TesteConstructor {
 	public static void main(String[] args) {
-		
-		
-	 Locale.setDefault(Locale.US);	
-	 Scanner input = new Scanner(System.in);
-	 Conta conta;
-	 
-	 System.out.println("Entre com o número da conta: ");
-	 String numConta = input.nextLine();
-	 System.out.println("Digite seu nome: ");
-     String titular = input.nextLine();
-	 
-	 conta = new Conta(0.0, numConta, titular);
-	 conta.imprimirDados();
+
+		Locale.setDefault(Locale.US);
+		Scanner input = new Scanner(System.in);
+		Conta conta;
+
+		System.out.println("Entre com o número da conta: ");
+		String numConta = input.nextLine();
+		System.out.println("Digite seu nome: ");
+		String titular = input.nextLine();
+
+		conta = new Conta(0.0, numConta, titular);
+		conta.imprimirDados();
 		System.out.println("\n--------------------");
-		
-		//entrada de dados para depositos
-		
+
+		// entrada de dados para depositos
+
 		System.out.println("\nDeseja Fazer alguma operação? 1 - Sim / 0 - Não : ");
 		int op = input.nextInt();
 		while (op == (1)) {
 			System.out.println("\nQual operação deseja fazer? D - Depósito / S - Saque : ");
-		    String operacao = input.next();
-		    System.out.println();
-	        if (operacao.equalsIgnoreCase("d")) {
-	        	System.out.println("\nPor favor entre com o deposito: ");
-	    		double valorDeposito = input.nextDouble();
-	    		conta.deposito(valorDeposito);
-	    		conta.imprimirDados();
-	    		System.out.println("\n--------------------");
-	        }
-	        else if (operacao.equalsIgnoreCase("s")) {
-	        	System.out.println("\nPor favor entre com o saque: ");
-	        	double valorSaque = input.nextDouble();
-	        	conta.saque(valorSaque);
-	        	conta.imprimirDados();
-	        	System.out.println("\n--------------------");
-	        	
-	        }
-	        
-	        else {
-	        	System.out.println("\nNão conseguimos indentificar o que deseja - OPERAÇÃO FINALIZADA");
-	        	break;
-	        }
-	        System.out.println("\nDeseja Fazer outra operação? 1 - Sim / 0 - Não : ");
-	        int oper = input.nextInt();
-	        if (oper != 1) {
-	        System.out.println();
-	        System.out.println("OPERAÇÃO FINALIZADA");
-	        break;
-	        
-	        }
+			String operacao = input.next();
+			System.out.println();
+			if (operacao.equalsIgnoreCase("d")) {
+				System.out.println("\nPor favor entre com o deposito: ");
+				double valorDeposito = input.nextDouble();
+				conta.deposito(valorDeposito);
+				conta.imprimirDados();
+				System.out.println("\n--------------------");
+			} else if (operacao.equalsIgnoreCase("s")) {
+				System.out.println("\nPor favor entre com o saque: ");
+				double valorSaque = input.nextDouble();
+				conta.saque(valorSaque);
+				conta.imprimirDados();
+				System.out.println("\n--------------------");
+
+			}
+
+			else {
+				System.out.println("\nNão conseguimos indentificar o que deseja - OPERAÇÃO FINALIZADA");
+				break;
+			}
+			System.out.println("\nDeseja Fazer outra operação? 1 - Sim / 0 - Não : ");
+			int oper = input.nextInt();
+			if (oper != 1) {
+				System.out.println();
+				System.out.println("OPERAÇÃO FINALIZADA");
+				break;
+
+			}
 		}
-		
+
 		input.close();
 	}
 }
-
