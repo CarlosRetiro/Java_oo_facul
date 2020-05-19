7d53d2575ba9b4bb987425f86f1220c86c96fd8b
CarlosRetiro <48919884+CarlosRetiro@users.noreply.github.com>
2020-05-18 21:11:15 -0300

    trabalho com interface GUI, loguin

---
+++ java_oo_facul/src/loguinGui/Edit_proile.java
@@ -0,0 +1,209 @@
+package loguinGui;
+
+import java.awt.Button;
+import java.awt.Component;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+@SuppressWarnings("serial")
+public class Edit_proile extends JFrame implements ActionListener
+{
+	public static String nome;
+	private JPanel contentPane;
+	private JTextField fldNome;
+	private JTextField fldSenha;
+	private JTextField fldContato;
+	private JTextField fldEmail;
+	private Component frame;
+	Button btnVoltar,btnEnviar;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) 
+	{
+		EventQueue.invokeLater(new Runnable() 
+		{
+			public void run() 
+			{
+				try {
+					View_profile frame = new View_profile();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Edit_proile() 
+	{
+		setTitle("Editar");
+		setResizable(false);
+		//System.out.println(name);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 386, 306);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JLabel lblPerfil = new JLabel("Editar perfil");
+		lblPerfil.setFont(new Font("Tahoma", Font.PLAIN, 36));
+		lblPerfil.setBounds(101, 11, 207, 44);
+		contentPane.add(lblPerfil);
+
+		JLabel lblNome = new JLabel("Nome");
+		lblNome.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNome.setBounds(10, 72, 49, 31);
+		contentPane.add(lblNome);
+
+		fldNome = new JTextField();
+		fldNome.setBounds(101, 79, 207, 20);
+		contentPane.add(fldNome);
+		fldNome.setColumns(10);
+
+
+		JLabel lblSenha = new JLabel("Senha");
+		lblSenha.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblSenha.setBounds(10, 114, 80, 20);
+		contentPane.add(lblSenha);
+
+		fldSenha = new JPasswordField();
+		fldSenha.setBounds(101, 110, 207, 20);
+		fldSenha.setEditable(true);
+		contentPane.add(fldSenha);
+		fldSenha.setColumns(10);
+
+		JLabel lblContato = new JLabel("Contato");
+		lblContato.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblContato.setBounds(10, 145, 88, 20);
+		contentPane.add(lblContato);
+
+		fldContato = new JTextField();
+		fldContato.setBounds(101, 141, 207, 20);
+		fldContato.setEditable(true);
+		contentPane.add(fldContato);
+		fldContato.setColumns(10);
+
+		JLabel lblEmail = new JLabel("Email");
+		lblEmail.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblEmail.setBounds(10, 183, 83, 14);
+		contentPane.add(lblEmail);
+
+		fldEmail = new JTextField();
+		fldEmail.setBounds(101, 182, 207, 20);
+		fldEmail.setEditable(true);
+		contentPane.add(fldEmail);
+		fldEmail.setColumns(10);
+
+		btnVoltar = new Button("Voltar");
+		btnVoltar.addActionListener(this);
+		btnVoltar.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnVoltar.setBounds(61, 228, 89, 23);
+		contentPane.add(btnVoltar);
+
+		btnEnviar = new Button("Enviar");
+		btnEnviar.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnEnviar.setBounds(172, 229, 89, 23);
+		btnEnviar.addActionListener(this);
+		contentPane.add(btnEnviar);
+
+		try
+		{
+			Class.forName("org.mariadb.jdbc.Driver");
+			Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/java_oo?user=root&password=");
+			Statement st=connection.createStatement();
+			ResultSet rs=st.executeQuery("select nome, senha, contato, email from perfil where nome='"+ nome +"'");
+			if(rs.next())
+			{
+				fldNome.setText(rs.getString(1));
+				fldSenha.setText(rs.getString(2));
+				fldContato.setText(rs.getString(3));
+				fldEmail.setText(rs.getString(4));
+
+			}
+		}
+		catch(ClassNotFoundException ce)
+		{
+			System.out.println("Dados não encontrados.");
+		}
+		catch(SQLException se)
+		{
+			se.printStackTrace();
+		}
+
+
+	}
+	
+	public Edit_proile(String n)
+	{
+		nome=n;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent ae)
+	{
+		Button bb=(Button)ae.getSource();
+		if(bb==btnEnviar)
+		{
+			if(fldSenha.getText().trim().isEmpty())
+			{
+				JOptionPane.showMessageDialog(frame,"Porfavor, é obrigatório criar uma senha.");
+			}
+			else
+			{
+				try
+				{
+					Class.forName("org.mariadb.jdbc.Driver");
+					Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/java_oo?user=root&password=");
+					PreparedStatement ps=connection.prepareStatement("update perfil set nome=(?),senha=(?), contato=(?), email=(?) where nome='"+nome+"'");
+					ps.setString(1,fldNome.getText());
+					ps.setString(2, fldSenha.getText());
+					ps.setString(3, fldContato.getText());
+					ps.setString(4, fldEmail.getText());
+					ps.executeUpdate();
+					JOptionPane.showMessageDialog(frame,"Atualização realizada com sucesso.");
+					dispose();
+					Login_Welcone m=new Login_Welcone(); 
+					m.setVisible(true);
+				}
+
+				catch(ClassNotFoundException ce)
+				{
+					System.out.println("Dados não encontrados.");
+				} 
+				catch(SQLException se)
+				{
+					se.printStackTrace();
+				}
+			}
+
+		}
+		if(bb==btnVoltar)
+		{
+			dispose();
+			Login_Welcone m=new Login_Welcone(); 
+			m.setVisible(true);
+		}
+
+	}
+}
---
+++ java_oo_facul/src/loguinGui/Login_Welcone.java
@@ -0,0 +1,136 @@
+package loguinGui;
+
+import java.awt.Button;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+
+@SuppressWarnings("serial")
+public class Login_Welcone extends JFrame implements ActionListener 
+{
+	public static String nome;
+	private JPanel contentPane;
+	Button btnVisualizar,btnEditar,btnLogOut;
+
+	public Login_Welcone(String n)
+	{
+		nome=n;
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Login_Welcone() 
+	{
+
+		/* INICIALIZANDO OS COMPONENTES */
+
+		//Criando os componentes		
+		contentPane = new JPanel();
+		JLabel lblHiwelcome = new JLabel("New text");
+		btnVisualizar = new Button("Visualizar Perfil");
+		btnEditar = new Button("Editar Perfil");
+		btnLogOut=new Button("Log Out");
+
+		/*------------------------------------------------*/
+
+		/* CONFIGURANDO DOS COMPONENTES */
+
+		//Configurando o frame
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		setContentPane(contentPane);
+
+		//Configurando o contentPane
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));		
+		contentPane.setLayout(null);
+
+		//Configurando tela de boas vindas
+		lblHiwelcome.setBounds(99, 11, 209, 62);
+		lblHiwelcome.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		String mssge="Olá  ";
+		mssge+=nome ;
+		mssge+="  Bem Vindo!!!";
+		lblHiwelcome.setText(mssge);
+
+
+		//Configurando o botão visualizar
+		btnVisualizar.setBounds(21, 111, 99, 44);
+		btnVisualizar.addActionListener(this);
+
+		//Configurando o botão editar
+		btnEditar.setBounds(127, 111, 106, 44);
+		btnEditar.addActionListener(this);
+
+		//Configurando o botão logOut
+		btnLogOut.addActionListener(this);
+		btnLogOut.setFont(new Font("Tahoma", Font.ITALIC, 10));
+		btnLogOut.setBounds(350, 11, 74, 19);
+
+		/*------------------------------------------------------*/
+
+
+		/* Adicionando os componentes do contentPane */
+
+		contentPane.add(lblHiwelcome);
+		contentPane.add(btnVisualizar);
+		contentPane.add(btnEditar);
+		contentPane.add(btnLogOut);
+
+		/*------------------------------------------------------*/
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent ae) 
+	{
+		Button bb=(Button)ae.getSource();
+		if(bb==btnVisualizar)
+		{
+			dispose();
+			new View_profile(nome);
+			View_profile le = new View_profile();  //View Profile
+
+			le.setVisible(true);
+
+		}
+		if(bb==btnEditar)
+		{
+			dispose();
+			new Edit_proile(nome);
+			Edit_proile ep=new Edit_proile();   //Edit Profile
+			ep.setVisible(true);
+		}
+
+		if(bb==btnLogOut)
+		{
+			dispose();
+			Loguin p=new Loguin();        //Logs us out ie. gets us back to Page1(Login page)
+			p.setVisible(true);
+		}
+
+	}
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login_Welcone frame = new Login_Welcone();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
